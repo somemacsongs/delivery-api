492e6895c8484043c489654a1a9075ae842b3f91
Giuliana Cabral <cabral.giu@hotmail.com>
2022-09-22 15:29:09 -0300

    Finalizado, tudo OK 22set 15h30

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 import express from "express";
-
 import * as dotenv from "dotenv";
 dotenv.config();
-
 import { dbConnect } from "./config/db.config.js";
 dbConnect();
+import { mealRouter } from "./routes/meal.routes.js";
 
 const app = express();
 
 app.use(express.json());
 
-// CONFIGUREM O SEUS ROTEADORES AQUI!
+app.use("/", mealRouter);
 
 app.listen(Number(process.env.PORT), () => {
-  console.log(`Server running at port ${process.env.PORT}`);
+  console.log(`Server running ok at port ${process.env.PORT}`);
 });
 
