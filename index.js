9efdaf1c7e5394b2a27a7ec8f6bf2e35b01bc0dd
Caroline Santos de Abreu <caroline@pop-os.localdomain>
2022-09-22 15:29:19 -0300

    first and final commit

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 import express from "express";
-
 import * as dotenv from "dotenv";
+import { dbConnect } from "./config/db.config.js";
+import { orderRouter } from "./routes/order.routes.js";
+
 dotenv.config();
 
-import { dbConnect } from "./config/db.config.js";
 dbConnect();
-
 const app = express();
 
 app.use(express.json());
 
 // CONFIGUREM O SEUS ROTEADORES AQUI!
+app.use("/order", orderRouter);
 
 app.listen(Number(process.env.PORT), () => {
   console.log(`Server up at port ${process.env.PORT}`);
--- routes/order.routes.js
+++ routes/order.routes.js
@@ -1,5 +1,72 @@
 import express from "express";
-
+import { OrderModel } from "../models/order.model.js";
 const orderRouter = express.Router();
+
+// CREATE
+orderRouter.post("/", async (req, res) => {
+  try {
+    const createdOrder = await OrderModel.create(req.body);
+
+    return res.status(201).json(createdOrder);
+  } catch (error) {
+    console.log(error);
+    return res.json(error);
+  }
+});
+
+// READ ALL
+orderRouter.get("/", async (req, res) => {
+  try {
+    const allOrders = await OrderModel.find();
+
+    return res.status(200).json(allOrders);
+  } catch (error) {
+    console.log(error);
+
+    return res.json(error);
+  }
+});
+
+// READ DETAILS
+orderRouter.get("/:id", async (req, res) => {
+  try {
+    const order = await OrderModel.findOne({ _id: req.params.id });
+
+    return res.status(200).json(order);
+  } catch (error) {
+    console.log(error);
+
+    return res.json(error);
+  }
+});
+
+// UPDATE
+orderRouter.put("/:id", async (req, res) => {
+  try {
+    const editOrder = await OrderModel.findOneAndUpdate(
+      { _id: req.params.id },
+      { ...req.body },
+      { new: true, runValidators: true }
+    );
+
+    return res.status(200).json(editOrder);
+  } catch (error) {
+    console.log(error);
+    return res.json(error);
+  }
+});
+
+// DELETE
+
+orderRouter.delete("/:id", async (req, res) => {
+  try {
+    const deletedOrder = await OrderModel.deleteOne({ _id: req.params.id });
+
+    return res.status(200).json(deletedOrder);
+  } catch (error) {
+    console.log(error);
+    return res.json(error);
+  }
+});
 
 export { orderRouter };
